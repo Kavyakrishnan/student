6af257439a67b7de9ae4a96cd25b9a51e4858c92
Kavyakrishnan <kavyakrishnan9821@gmail.com>
2022-05-16 21:09:28 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import Addstudent from './Component/Addstudent';
+import Addstudent1 from './Component/Addstudent1';
 
 function App() {
   return (
     <>
-    <Addstudent/>
+   <Addstudent1/>
     </>
+    
   );
 }
 
